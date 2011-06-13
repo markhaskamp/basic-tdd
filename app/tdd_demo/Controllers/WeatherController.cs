4f9cb95e332df950d2b440643d7d6a0d465e5af6
Mark Haskamp <mark.haskamp@pinnsg.com>
2011-06-13 15:07:35 -0400

    one working test

---
+++ app/tdd_demo/Controllers/WeatherController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace tdd_demo.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public JsonResult County(string countyName)
+        {
+            return Json("eddie would go", "text/html");
+        }
+
+    }
+}
---
+++ test/tdd_demo_test/WeatherControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tdd_demo.Controllers;
+
+namespace tdd_demo_test
+{
+    [TestClass]
+    public class WeatherControllerTests
+    {
+        [TestMethod]
+        public void County_method_returns_json() {
+            WeatherController controller = new WeatherController();
+            ActionResult actionResult = controller.County("Montgomery");
+
+            Assert.AreEqual(typeof (JsonResult), actionResult.GetType());
+        }
+    }
+}
